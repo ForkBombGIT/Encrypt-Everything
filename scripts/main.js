0775368446952d53a6656bf6b3f3d81580235459
Eros <erosdipede@cmail.carleton.ca>
2018-08-22 16:00:39 -0400

    added button to move output to input

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,20 @@
-$( document ).ready(function(){
+$(document).ready(function() {
   if ($(this).find(":selected").text() == "ROT") $("#rotamount").removeAttr("hidden")
-  else $("#rotamount").attr("hidden","hidden");
+  else $("#rotamount").attr("hidden", "hidden");
 
   if ($(this).find(":selected").text() == "Rail Fence") $("#railamount").removeAttr("hidden")
-  else $("#railamount").attr("hidden","hidden");
+  else $("#railamount").attr("hidden", "hidden");
 
   $("#outputbox").val("");
   $("#inputbox").val("");
 });
 
-$("#cipherselection").on('change', function(){
+$("#cipherselection").on('change', function() {
   if ($(this).find(":selected").text() == "ROT") $("#rotamount").removeAttr("hidden")
-  else $("#rotamount").attr("hidden","hidden");
+  else $("#rotamount").attr("hidden", "hidden");
 
   if ($(this).find(":selected").text() == "Rail Fence") $("#railamount").removeAttr("hidden")
-  else $("#railamount").attr("hidden","hidden");
+  else $("#railamount").attr("hidden", "hidden");
 });
 
 //handles submit button
@@ -24,21 +24,26 @@ $("#encryptbtn").on("click", function() {
     var input = $("#inputbox").val();
     if (input != "") {
       $("#inputbox").val("");
-      switch(cipher){
+      switch (cipher) {
         case ("ROT"):
-          $("#outputbox").val(rotEncrypt(input,parseInt($("#numberofrot").val()),1));
+          $("#outputbox").val(rotEncrypt(input, parseInt($("#numberofrot").val()), 1));
           break;
         case ("Baconian"):
-          $("#outputbox").val(baconianEncrypt(input,1));
+          $("#outputbox").val(baconianEncrypt(input, 1));
           break;
         case ("Rail Fence"):
-          $("#outputbox").val(railfenceEncrypt(input,parseInt($("#numberofrail").val()),1));
+          $("#outputbox").val(railfenceEncrypt(input, parseInt($("#numberofrail").val()), 1));
           break;
         default:
           break;
       }
     }
   }
+});
+
+$("#switchbtn").on("click", function() {
+  $("#inputbox").val($("#outputbox").val());
+  $("#outputbox").val("")
 });
 
 //handles submit button
@@ -48,15 +53,15 @@ $("#decryptbtn").on("click", function() {
     var input = $("#inputbox").val();
     if (input != "") {
       $("#inputbox").val("");
-      switch(cipher){
+      switch (cipher) {
         case ("ROT"):
-          $("#outputbox").val(rotEncrypt(input,parseInt($("#numberofrot").val()),0));
+          $("#outputbox").val(rotEncrypt(input, parseInt($("#numberofrot").val()), 0));
           break;
         case ("Baconian"):
-          $("#outputbox").val(baconianEncrypt(input,0));
+          $("#outputbox").val(baconianEncrypt(input, 0));
           break;
         case ("Rail Fence"):
-          $("#outputbox").val(railfenceEncrypt(input,parseInt($("#numberofrail").val()),0));
+          $("#outputbox").val(railfenceEncrypt(input, parseInt($("#numberofrail").val()), 0));
           break;
         default:
           break;
