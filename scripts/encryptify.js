b4d6b980d769c3399ff1178f849a18b2712e5fbf
Eros <eros.dipede@cloud.statcan.ca>
2018-11-06 20:14:01 +0000

    Cleaned up styles

--- scripts/encryptify.js
+++ scripts/encryptify.js
@@ -1,97 +1,129 @@
 function rotEncrypt(val, rot, enc) {
   var newString = "";
   for (var i = 0; i < val.length; i++) {
-    if (isNaN(parseInt(val.charAt(i)), 10) && !(val.charAt(i).match(/^[.,'{}!@#$%^&*()/\-=+_;"<>~:!?]/)) && !(val.charAt(i) == ' ' ||
-        val.charAt(i) == '\t' ||
-        val.charAt(i) == '\n' ||
-        val.charAt(i) == '\r')) {
-      minVal = (val.charAt(i) == val.charAt(i).toUpperCase()) ? 65 : 97;
-      maxVal = (val.charAt(i) == val.charAt(i).toUpperCase()) ? 90 : 122;
-      newString += ((enc) ? (((val.charCodeAt(i) + rot) <= maxVal) ? (String.fromCharCode(val.charCodeAt(i) + rot)) :
-                                                                     (String.fromCharCode((minVal + ((val.charCodeAt(i) + rot) - maxVal)) - 1))) :
-                             ((val.charCodeAt(i) - rot) >= minVal) ? (String.fromCharCode(val.charCodeAt(i) - rot)) :
-                                                                     (String.fromCharCode(maxVal - (minVal - (val.charCodeAt(i) - rot)) + 1)));
+    if (
+      isNaN(parseInt(val.charAt(i)), 10) &&
+      !val.charAt(i).match(/^[.,'{}!@#$%^&*()/\-=+_;"<>~:!?]/) &&
+      !(
+        val.charAt(i) == " " ||
+        val.charAt(i) == "\t" ||
+        val.charAt(i) == "\n" ||
+        val.charAt(i) == "\r"
+      )
+    ) {
+      minVal = val.charAt(i) == val.charAt(i).toUpperCase() ? 65 : 97;
+      maxVal = val.charAt(i) == val.charAt(i).toUpperCase() ? 90 : 122;
+      newString += enc
+        ? val.charCodeAt(i) + rot <= maxVal
+          ? String.fromCharCode(val.charCodeAt(i) + rot)
+          : String.fromCharCode(minVal + (val.charCodeAt(i) + rot - maxVal) - 1)
+        : val.charCodeAt(i) - rot >= minVal
+          ? String.fromCharCode(val.charCodeAt(i) - rot)
+          : String.fromCharCode(
+              maxVal - (minVal - (val.charCodeAt(i) - rot)) + 1
+            );
     } else newString += val.charAt(i);
   }
   return newString;
-
 }
 
 function baconianEncrypt(val, enc) {
   var newString = "";
   var baconianConvertions = {
-    'a': 'aaaaa','b': 'aaaab',
-    'c': 'aaaba','d': 'aaabb',
-    'e': 'aabaa','f': 'aabab',
-    'g': 'aabba','h': 'aabbb',
-    'i': 'abaaa','j': 'abaab',
-    'k': 'ababa','l': 'ababb',
-    'm': 'abbaa','n': 'abbab',
-    'o': 'abbba','p': 'abbbb',
-    'q': 'baaaa','r': 'baaab',
-    's': 'baaba','t': 'baabb',
-    'u': 'babaa','v': 'babab',
-    'w': 'babba','x': 'babbb',
-    'y': 'bbaaa','z': 'bbaab'
-  }
+    a: "aaaaa",
+    b: "aaaab",
+    c: "aaaba",
+    d: "aaabb",
+    e: "aabaa",
+    f: "aabab",
+    g: "aabba",
+    h: "aabbb",
+    i: "abaaa",
+    j: "abaab",
+    k: "ababa",
+    l: "ababb",
+    m: "abbaa",
+    n: "abbab",
+    o: "abbba",
+    p: "abbbb",
+    q: "baaaa",
+    r: "baaab",
+    s: "baaba",
+    t: "baabb",
+    u: "babaa",
+    v: "babab",
+    w: "babba",
+    x: "babbb",
+    y: "bbaaa",
+    z: "bbaab"
+  };
 
   for (var i = 0; i < val.length; i++) {
-    if (isNaN(parseInt(val.charAt(i)), 10) && !(val.charAt(i).match(/^[.,'{}!@#$%^&*()/\-=+_;"<>~:!?]/)) && !(val.charAt(i) == ' ' ||
-        val.charAt(i) == '\t' ||
-        val.charAt(i) == '\n' ||
-        val.charAt(i) == '\r')) {
-        newString += ((enc) ? baconianConvertions[val.charAt(i).toLowerCase()] :  _.findKey(baconianConvertions, function(v) { return v === val.substring(i,i+5);  }));
-        if (!(enc)) i+= 4;
+    if (
+      isNaN(parseInt(val.charAt(i)), 10) &&
+      !val.charAt(i).match(/^[.,'{}!@#$%^&*()/\-=+_;"<>~:!?]/) &&
+      !(
+        val.charAt(i) == " " ||
+        val.charAt(i) == "\t" ||
+        val.charAt(i) == "\n" ||
+        val.charAt(i) == "\r"
+      )
+    ) {
+      newString += enc
+        ? baconianConvertions[val.charAt(i).toLowerCase()]
+        : _.findKey(baconianConvertions, function(v) {
+            return v === val.substring(i, i + 5);
+          });
+      if (!enc) i += 4;
     } else newString += val.charAt(i);
   }
   return newString;
 }
 
-function railfenceEncrypt(val,rail,enc){
-  var rails = {}
+function railfenceEncrypt(val, rail, enc) {
+  var rails = {};
   var index = 0;
   var increase = true;
-  var newString = ""
+  var newString = "";
 
-  if (enc){
-    for (var i = 0; i < val.length; i++){
+  if (enc) {
+    for (var i = 0; i < val.length; i++) {
       if (!(index in rails)) rails[index] = "";
       rails[index] += val.charAt(i);
 
-      index += (increase) ? 1 : -1;
-      if (index >= (rail - 1)) increase = false;
+      index += increase ? 1 : -1;
+      if (index >= rail - 1) increase = false;
       else if (index == 0) increase = true;
     }
+  } else {
+    if (val.length <= rail) return val;
   }
-  else{
-    if (val.length <= rail)
-      return val;
-  }
-  return function(){
-    for (var i = 0; i < rail; i++){if (i < val.length) newString += rails[i]}
+  return function() {
+    for (var i = 0; i < rail; i++) {
+      if (i < val.length) newString += rails[i];
+    }
     return newString;
-  }
+  };
 }
 
-function columnarEncrypt(val,key,enc){
+function columnarEncrypt(val, key, enc) {
   index = 0;
-  var cols = {}
+  var cols = {};
 
-  for (var i = 0; i < key.length; i++){
+  for (var i = 0; i < key.length; i++) {
     cols[key.charAt(i) + i.toString()] = "";
   }
 
-  for (var i = 0; i < val.length; i++){
+  for (var i = 0; i < val.length; i++) {
     if (index == key.length) index = 0;
     cols[key.charAt(index) + index.toString()] += val.charAt(i);
     index++;
   }
 
-  var keys = (Object.keys(cols).sort())
-  return function(){
-    newString = ""
-    for (var i = 0; i < keys.length; i++)
-      newString += cols[keys[i]];
+  var keys = Object.keys(cols).sort();
+  return function() {
+    newString = "";
+    for (var i = 0; i < keys.length; i++) newString += cols[keys[i]];
     return newString;
-  }
+  };
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,35 @@
 //clears inputs
 $(document).ready(function() {
-  if ($(this).find(":selected").text() == "ROT") $("#rotamount").removeAttr("hidden")
+  if (
+    $(this)
+      .find(":selected")
+      .text() == "ROT"
+  )
+    $("#rotamount").removeAttr("hidden");
   else $("#rotamount").attr("hidden", "hidden");
 
-  if ($(this).find(":selected").text() == "Baconian") $("#baconianinfo").removeAttr("hidden")
+  if (
+    $(this)
+      .find(":selected")
+      .text() == "Baconian"
+  )
+    $("#baconianinfo").removeAttr("hidden");
   else $("#baconianinfo").attr("hidden", "hidden");
 
-  if ($(this).find(":selected").text() == "Rail Fence") $("#railamount").removeAttr("hidden")
+  if (
+    $(this)
+      .find(":selected")
+      .text() == "Rail Fence"
+  )
+    $("#railamount").removeAttr("hidden");
   else $("#railamount").attr("hidden", "hidden");
 
-  if ($(this).find(":selected").text() == "Columnar Transposition") $("#colkeyinput").removeAttr("hidden")
+  if (
+    $(this)
+      .find(":selected")
+      .text() == "Columnar Transposition"
+  )
+    $("#colkeyinput").removeAttr("hidden");
   else $("#colkeyinput").attr("hidden", "hidden");
 
   $("#outputbox").val("");
@@ -17,45 +37,77 @@ $(document).ready(function() {
 });
 
 //event handler on drop down
-$("#cipherselection").on('change', function() {
-  if ($(this).find(":selected").text() == "ROT") $("#rotamount").removeAttr("hidden")
+$("#cipherselection").on("change", function() {
+  if (
+    $(this)
+      .find(":selected")
+      .text() == "ROT"
+  )
+    $("#rotamount").removeAttr("hidden");
   else $("#rotamount").attr("hidden", "hidden");
 
-  if ($(this).find(":selected").text() == "Baconian") $("#baconianinfo").removeAttr("hidden")
+  if (
+    $(this)
+      .find(":selected")
+      .text() == "Baconian"
+  )
+    $("#baconianinfo").removeAttr("hidden");
   else $("#baconianinfo").attr("hidden", "hidden");
 
-  if ($(this).find(":selected").text() == "Rail Fence") $("#railamount").removeAttr("hidden")
+  if (
+    $(this)
+      .find(":selected")
+      .text() == "Rail Fence"
+  )
+    $("#railamount").removeAttr("hidden");
   else $("#railamount").attr("hidden", "hidden");
 
-  if ($(this).find(":selected").text() == "Columnar Transposition") $("#colkeyinput").removeAttr("hidden")
+  if (
+    $(this)
+      .find(":selected")
+      .text() == "Columnar Transposition"
+  )
+    $("#colkeyinput").removeAttr("hidden");
   else $("#colkeyinput").attr("hidden", "hidden");
 });
 
 //handles submit button
 $("#encryptbtn").on("click", function() {
-  var cipher = $("#cipherselection").find(":selected").text();
+  var cipher = $("#cipherselection")
+    .find(":selected")
+    .text();
   if (cipher != "") {
     var input = $("#inputbox").val();
     if (input != "") {
       switch (cipher) {
-        case ("ROT"):
-          if (!isNaN(parseInt($("#numberofrot").val())) || $("#numberofrot").val() != ""){
-            $("#outputbox").val(rotEncrypt(input, parseInt($("#numberofrot").val()), 1));
+        case "ROT":
+          if (
+            !isNaN(parseInt($("#numberofrot").val())) ||
+            $("#numberofrot").val() != ""
+          ) {
+            $("#outputbox").val(
+              rotEncrypt(input, parseInt($("#numberofrot").val()), 1)
+            );
             $("#inputbox").val("");
           }
           break;
-        case ("Baconian"):
+        case "Baconian":
           $("#outputbox").val(baconianEncrypt(input, 1));
           $("#inputbox").val("");
           break;
-        case ("Rail Fence"):
-          if (!isNaN(parseInt($("#numberofrail").val())) || $("#numberofrail").val() != ""){
-            $("#outputbox").val(railfenceEncrypt(input, parseInt($("#numberofrail").val()), 1));
+        case "Rail Fence":
+          if (
+            !isNaN(parseInt($("#numberofrail").val())) ||
+            $("#numberofrail").val() != ""
+          ) {
+            $("#outputbox").val(
+              railfenceEncrypt(input, parseInt($("#numberofrail").val()), 1)
+            );
             $("#inputbox").val("");
           }
           break;
-        case("Columnar Transposition"):
-          if ($("#colkey").val() != ""){
+        case "Columnar Transposition":
+          if ($("#colkey").val() != "") {
             $("#outputbox").val(columnarEncrypt(input, $("#colkey").val(), 1));
             $("#inputbox").val("");
           }
@@ -64,8 +116,8 @@ $("#encryptbtn").on("click", function() {
           break;
       }
     }
-  } else{
-    if ($("#inputbox").val() > 0){
+  } else {
+    if ($("#inputbox").val() > 0) {
       $("#outputbox").val($("#inputbox").val());
       $("#inputbox").val("");
     }
@@ -74,39 +126,57 @@ $("#encryptbtn").on("click", function() {
 
 //handles output to input button
 $("#switchbtn").on("click", function() {
-  if ($("#outputbox").val().length != 0){
-      $("#inputbox").val($("#outputbox").val());
-      $("#outputbox").val("")
+  if ($("#outputbox").val().length != 0) {
+    $("#inputbox").val($("#outputbox").val());
+    $("#outputbox").val("");
 
-      $("#decryptbtn").focus();
-    }
+    $("#decryptbtn").focus();
+  }
 });
 
 //handles submit button
 $("#decryptbtn").on("click", function() {
-  var cipher = $("#cipherselection").find(":selected").text();
+  var cipher = $("#cipherselection")
+    .find(":selected")
+    .text();
   if (cipher != "") {
     var input = $("#inputbox").val();
     if (input != "") {
       switch (cipher) {
-        case ("ROT"):
-          if (!isNaN(parseInt($("#numberofrot").val())) || $("#numberofrot").val() != ""){
-            $("#outputbox").val(rotEncrypt(input, parseInt($("#numberofrot").val()), 0));
+        case "ROT":
+          if (
+            !isNaN(parseInt($("#numberofrot").val())) ||
+            $("#numberofrot").val() != ""
+          ) {
+            $("#outputbox").val(
+              rotEncrypt(input, parseInt($("#numberofrot").val()), 0)
+            );
             $("#inputbox").val("");
           }
           break;
-        case ("Baconian"):
+        case "Baconian":
           $("#outputbox").val(baconianEncrypt(input, 0));
           $("#inputbox").val("");
+          break;
+        case "Rail Fence":
+          if (
+            !isNaN(parseInt($("#numberofrail").val())) ||
+            $("#numberofrail").val() != ""
+          ) {
+            $("#outputbox").val(
+              railfenceEncrypt(input, parseInt($("#numberofrail").val()), 0)
+            );
+            $("#inputbox").val("");
+          }
           break;
         default:
           break;
       }
     }
   } else {
-      if ($("#inputbox").val() > 0){
-        $("#outputbox").val($("#inputbox").val());
-        $("#inputbox").val("");
-      }
+    if ($("#inputbox").val() > 0) {
+      $("#outputbox").val($("#inputbox").val());
+      $("#inputbox").val("");
+    }
   }
 });
